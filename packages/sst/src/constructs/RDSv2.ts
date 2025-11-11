aa111437a261fdd27f7e6a7d8ccae69aca6cb6f4
GuyMad232 <guy.maduel@post.runi.ac.il>
2025-11-10 16:33:55 -0800

    added pg17 verison

--- packages/sst/src/constructs/RDSv2.ts
+++ packages/sst/src/constructs/RDSv2.ts
@@ -51,7 +51,8 @@ export interface RDSv2Props {
     | 'postgresql13.9'
     | 'postgresql14.10'
     | 'postgresql15.12'
-    | 'postgresql16.1';
+    | 'postgresql16.1'
+    | 'postgresql17.4';
   defaultDatabaseName: string;
   scaling?: {
     minCapacity?: keyof typeof AuroraCapacityUnit | number;
@@ -220,6 +221,7 @@ export class RDSv2 extends Construct implements SSTConstruct {
       'postgresql14.10': AuroraPostgresEngineVersion.VER_14_10,
       'postgresql15.12': AuroraPostgresEngineVersion.VER_15_12,
       'postgresql16.1': AuroraPostgresEngineVersion.VER_16_1,
+      'postgresql17.4': AuroraPostgresEngineVersion.VER_17_4,
     };
     return engine.includes('mysql')
       ? DatabaseClusterEngine.auroraMysql({ version: versions[engine] })
